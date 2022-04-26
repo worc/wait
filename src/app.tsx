4ceb7203fbe873120d0b6e0c7b95a39ca03dc4a8
Curtis McAllister <cmcallister@newrelic.com>
2022-04-26 14:48:15 -0700

    setup a demo in the app component

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,37 @@
 import React from 'react'
+import styled from 'styled-components'
+
+import Live from './strategies/live'
+import Debounce from './strategies/debounce'
+import Throttle from './strategies/throttle'
+import Delay from './strategies/delay'
+
+const Demo = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  gap: 24px;
+  
+  & > div {
+    display: flex;
+    flex-flow: column nowrap;
+    gap: 8px;
+  }
+`
 
 export default function App () {
+  const timeout = 1000
+
   return (
-    <h1>Hello world</h1>
+    <>
+      <h1 style={{ textAlign: 'center' }}>Live vs Debounced vs Throttled vs Delayed</h1>
+
+      <Demo>
+        <Live/>
+        <Debounce timeout={ timeout }/>
+        <Delay timeout={ timeout }/>
+        <Throttle timeout={ timeout }/>
+      </Demo>
+
+    </>
   )
 }
