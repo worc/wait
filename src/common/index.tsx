1eb075c017c5b4390f798043dc5e289351232cf0
Curtis McAllister <cmcallister@newrelic.com>
2022-04-26 14:46:44 -0700

    add common styled components

---
+++ src/common/index.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const TargetArea = styled.div`
+  align-items: center;
+  border: 1px solid #ace;
+  display: flex;
+  height: 80px;
+  justify-content: center;
+  width: 80px;
+`
+
+export const EventsContainer = styled.div`
+  display: flex;
+  gap: 8px;
+  height: 80px;
+`
+
+export const EventMarker = styled.div`
+  background-color: darkgreen;
+  width: 12px;
+`
